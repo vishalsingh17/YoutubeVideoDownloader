7a51a6d768d5f0837e2fb086fe99cc3261763191
unknown <vishal170997@gmail.com>
2022-06-10 12:16:41 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 import streamlit as st
-from yt_extractor import get_video
+from pytube import YouTube
 
-st.title("Download your favorite video HERE!!")
+st.title("Download your favourite video HERE!!")
+DOWNLOAD_PATH = './downloads/'
+video_link = st.text_input("Enter the link of the video you want to download").split(',')
+for i in video_link:
+    YouTube(i).streams.first().download()
+    yt = YouTube(i)
+    stream = yt.streams.filter(progressive=True, file_extension='mp4')
+    stream.download()
+    # mp4files = yt.streams.filter('mp4')
+    # d_video = yt.get(mp4files[-1].extension,mp4files[-1].resolution)
 
-url = st.text_input("URL goes here", URL_goes_here)
+    # try:
+    #     yt.download(DOWNLOAD_PATH)
+    # except:
+    #     st.write("There is some Error!")  
+st.write('Videos Download Successfully!')
